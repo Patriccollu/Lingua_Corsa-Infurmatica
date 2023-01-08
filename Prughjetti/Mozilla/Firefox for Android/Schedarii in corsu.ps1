f1a52f7bf2702f6fa2fdffb0744d1da7d2bd6ec8
Patriccollu <patriccollu@gmail.com>
2023-01-08 14:50:57 +0100

    Scaricà tutti i schedarii tradutti in corsu

--- Prughjetti/Mozilla/Firefox for Android/Schedarii in corsu.ps1
+++ Prughjetti/Mozilla/Firefox for Android/Schedarii in corsu.ps1
@@ -1,69 +1,35 @@
-Invoke-WebRequest -Uri "https://github.com/mozilla-l10n/android-l10n/raw/master/mozilla-mobile/android-components/components/browser/awesomebar/src/main/res/values-co/strings.xml" -OutFile "components-browser-awesomebar-strings.xml"
-
-Invoke-WebRequest -Uri "https://github.com/mozilla-l10n/android-l10n/raw/master/mozilla-mobile/android-components/components/browser/engine-system/src/main/res/values-co/strings.xml" -OutFile "components-browser-engine-system-strings.xml"
-
-Invoke-WebRequest -Uri "https://github.com/mozilla-l10n/android-l10n/raw/master/mozilla-mobile/android-components/components/browser/errorpages/src/main/res/values-co/strings.xml" -OutFile "components-browser-errorpages-strings.xml"
-
-Invoke-WebRequest -Uri "https://github.com/mozilla-l10n/android-l10n/raw/master/mozilla-mobile/android-components/components/browser/menu2/src/main/res/values-co/strings.xml" -OutFile "components-browser-menu-strings.xml"
-
-Invoke-WebRequest -Uri "https://github.com/mozilla-l10n/android-l10n/raw/master/mozilla-mobile/android-components/components/browser/menu/src/main/res/values-co/strings.xml" -OutFile "components-browser-menu2-strings.xml"
-
-Invoke-WebRequest -Uri "https://github.com/mozilla-l10n/android-l10n/raw/master/mozilla-mobile/android-components/components/browser/toolbar/src/main/res/values-co/strings.xml" -OutFile "components-browser-toolbar-strings.xml"
-
-Invoke-WebRequest -Uri "https://github.com/mozilla-l10n/android-l10n/raw/master/mozilla-mobile/android-components/components/compose/awesomebar/src/main/res/values-co/strings.xml" -OutFile "components-compose-awesomebar-strings.xml"
-
-Invoke-WebRequest -Uri "https://github.com/mozilla-l10n/android-l10n/raw/master/mozilla-mobile/android-components/components/compose/browser-toolbar/src/main/res/values-co/strings.xml" -OutFile "components-compose-browser-toolbar-strings.xml"
-
-Invoke-WebRequest -Uri "https://github.com/mozilla-l10n/android-l10n/raw/master/mozilla-mobile/android-components/components/compose/cfr/src/main/res/values-co/strings.xml" -OutFile "components-compose-cfr-strings.xml"
-
-Invoke-WebRequest -Uri "https://github.com/mozilla-l10n/android-l10n/raw/master/mozilla-mobile/android-components/components/compose/tabstray/src/main/res/values-co/strings.xml" -OutFile "components-compose-tabstray-strings.xml"
-
-Invoke-WebRequest -Uri "https://github.com/mozilla-l10n/android-l10n/raw/master/mozilla-mobile/android-components/components/feature/addons/src/main/res/values-co/strings.xml" -OutFile "components-feature-addons-strings.xml"
-
-Invoke-WebRequest -Uri "https://github.com/mozilla-l10n/android-l10n/raw/master/mozilla-mobile/android-components/components/feature/app-links/src/main/res/values-co/strings.xml" -OutFile "components-feature-app-links-strings.xml"
-
-Invoke-WebRequest -Uri "https://github.com/mozilla-l10n/android-l10n/raw/master/mozilla-mobile/android-components/components/feature/autofill/src/main/res/values-co/strings.xml" -OutFile "components-feature-autofill-strings.xml"
-
-Invoke-WebRequest -Uri "https://github.com/mozilla-l10n/android-l10n/raw/master/mozilla-mobile/android-components/components/feature/awesomebar/src/main/res/values-co/strings.xml" -OutFile "components-feature-awesomebar-strings.xml"
-
-Invoke-WebRequest -Uri "https://github.com/mozilla-l10n/android-l10n/raw/master/mozilla-mobile/android-components/components/feature/contextmenu/src/main/res/values-co/strings.xml" -OutFile "components-feature-contextmenu-strings.xml"
-
-Invoke-WebRequest -Uri "https://github.com/mozilla-l10n/android-l10n/raw/master/mozilla-mobile/android-components/components/feature/customtabs/src/main/res/values-co/strings.xml" -OutFile "components-feature-customtabs-strings.xml"
-
-Invoke-WebRequest -Uri "https://github.com/mozilla-l10n/android-l10n/raw/master/mozilla-mobile/android-components/components/feature/downloads/src/main/res/values-co/strings.xml" -OutFile "components-feature-downloads-strings.xml"
-
-Invoke-WebRequest -Uri "https://github.com/mozilla-l10n/android-l10n/raw/master/mozilla-mobile/android-components/components/feature/findinpage/src/main/res/values-co/strings.xml" -OutFile "components-feature-findinpage-strings.xml"
-
-Invoke-WebRequest -Uri "https://github.com/mozilla-l10n/android-l10n/raw/master/mozilla-mobile/android-components/components/feature/media/src/main/res/values-co/strings.xml" -OutFile "components-feature-media-strings.xml"
-
-Invoke-WebRequest -Uri "https://github.com/mozilla-l10n/android-l10n/raw/master/mozilla-mobile/android-components/components/feature/privatemode/src/main/res/values-co/strings.xml" -OutFile "components-feature-privatemode-strings.xml"
-
-Invoke-WebRequest -Uri "https://github.com/mozilla-l10n/android-l10n/raw/master/mozilla-mobile/android-components/components/feature/prompts/src/main/res/values-co/strings.xml" -OutFile "components-feature-prompts-strings.xml"
-
-Invoke-WebRequest -Uri "https://github.com/mozilla-l10n/android-l10n/raw/master/mozilla-mobile/android-components/components/feature/pwa/src/main/res/values-co/strings.xml" -OutFile "components-feature-pwa-strings.xml"
-
-Invoke-WebRequest -Uri "https://github.com/mozilla-l10n/android-l10n/raw/master/mozilla-mobile/android-components/components/feature/qr/src/main/res/values-co/strings.xml" -OutFile "components-feature-qr-strings.xml"
-
-Invoke-WebRequest -Uri "https://github.com/mozilla-l10n/android-l10n/raw/master/mozilla-mobile/android-components/components/feature/readerview/src/main/res/values-co/strings.xml" -OutFile "components-feature-readerview-strings.xml"
-
-Invoke-WebRequest -Uri "https://github.com/mozilla-l10n/android-l10n/raw/master/mozilla-mobile/android-components/components/feature/search/src/main/res/values-co/strings.xml" -OutFile "components-feature-search-strings.xml"
-
-Invoke-WebRequest -Uri "https://github.com/mozilla-l10n/android-l10n/raw/master/mozilla-mobile/android-components/components/feature/sitepermissions/src/main/res/values-co/strings.xml" -OutFile "components-feature-sitepermissions-strings.xml"
-
-Invoke-WebRequest -Uri "https://github.com/mozilla-l10n/android-l10n/raw/master/mozilla-mobile/android-components/components/feature/tabs/src/main/res/values-co/strings.xml" -OutFile "components-feature-tabs-strings.xml"
-
-Invoke-WebRequest -Uri "https://github.com/mozilla-l10n/android-l10n/raw/master/mozilla-mobile/android-components/components/feature/webnotifications/src/main/res/values-co/strings.xml" -OutFile "components-feature-webnotifications-strings.xml"
-
-Invoke-WebRequest -Uri "https://github.com/mozilla-l10n/android-l10n/raw/master/mozilla-mobile/android-components/components/lib/crash/src/main/res/values-co/strings.xml" -OutFile "components-lib-crash-strings.xml"
-
-Invoke-WebRequest -Uri "https://github.com/mozilla-l10n/android-l10n/raw/master/mozilla-mobile/android-components/components/service/nimbus/src/main/res/values-co/strings.xml" -OutFile "components-service-nimbus-strings.xml"
-
-Invoke-WebRequest -Uri "https://github.com/mozilla-l10n/android-l10n/raw/master/mozilla-mobile/android-components/components/support/base/src/main/res/values-co/strings.xml" -OutFile "components-support-base-strings.xml"
-
-Invoke-WebRequest -Uri "https://github.com/mozilla-l10n/android-l10n/raw/master/mozilla-mobile/android-components/components/support/ktx/src/main/res/values-co/strings.xml" -OutFile "components-support-ktx-strings.xml"
-
-Invoke-WebRequest -Uri "https://github.com/mozilla-l10n/android-l10n/raw/master/mozilla-mobile/android-components/components/support/migration/src/main/res/values-co/strings.xml" -OutFile "components-support-migration-strings.xml"
-
-Invoke-WebRequest -Uri "https://github.com/mozilla-l10n/android-l10n/raw/master/mozilla-mobile/android-components/components/ui/tabcounter/src/main/res/values-co/strings.xml" -OutFile "components-ui-tabcounter-strings.xml"
-
-Invoke-WebRequest -Uri "https://github.com/mozilla-l10n/android-l10n/raw/master/mozilla-mobile/fenix/app/src/main/res/values-co/strings.xml" -OutFile "fenix-strings.xml"
+Invoke-WebRequest -Uri https://github.com/mozilla-l10n/android-l10n/raw/master/mozilla-mobile/android-components/components/browser/awesomebar/src/main/res/values-co/strings.xml -OutFile D:\Temp\components-browser-awesomebar-strings.xml
+Invoke-WebRequest -Uri https://github.com/mozilla-l10n/android-l10n/raw/master/mozilla-mobile/android-components/components/browser/engine-system/src/main/res/values-co/strings.xml -OutFile D:\Temp\components-browser-engine-system-strings.xml
+Invoke-WebRequest -Uri https://github.com/mozilla-l10n/android-l10n/raw/master/mozilla-mobile/android-components/components/browser/errorpages/src/main/res/values-co/strings.xml -OutFile D:\Temp\components-browser-errorpages-strings.xml
+Invoke-WebRequest -Uri https://github.com/mozilla-l10n/android-l10n/raw/master/mozilla-mobile/android-components/components/browser/menu2/src/main/res/values-co/strings.xml -OutFile D:\Temp\components-browser-menu-strings.xml
+Invoke-WebRequest -Uri https://github.com/mozilla-l10n/android-l10n/raw/master/mozilla-mobile/android-components/components/browser/menu/src/main/res/values-co/strings.xml -OutFile D:\Temp\components-browser-menu2-strings.xml
+Invoke-WebRequest -Uri https://github.com/mozilla-l10n/android-l10n/raw/master/mozilla-mobile/android-components/components/browser/toolbar/src/main/res/values-co/strings.xml -OutFile D:\Temp\components-browser-toolbar-strings.xml
+Invoke-WebRequest -Uri https://github.com/mozilla-l10n/android-l10n/raw/master/mozilla-mobile/android-components/components/compose/awesomebar/src/main/res/values-co/strings.xml -OutFile D:\Temp\components-compose-awesomebar-strings.xml
+Invoke-WebRequest -Uri https://github.com/mozilla-l10n/android-l10n/raw/master/mozilla-mobile/android-components/components/compose/browser-toolbar/src/main/res/values-co/strings.xml -OutFile D:\Temp\components-compose-browser-toolbar-strings.xml
+Invoke-WebRequest -Uri https://github.com/mozilla-l10n/android-l10n/raw/master/mozilla-mobile/android-components/components/compose/cfr/src/main/res/values-co/strings.xml -OutFile D:\Temp\components-compose-cfr-strings.xml
+Invoke-WebRequest -Uri https://github.com/mozilla-l10n/android-l10n/raw/master/mozilla-mobile/android-components/components/compose/tabstray/src/main/res/values-co/strings.xml -OutFile D:\Temp\components-compose-tabstray-strings.xml
+Invoke-WebRequest -Uri https://github.com/mozilla-l10n/android-l10n/raw/master/mozilla-mobile/android-components/components/feature/addons/src/main/res/values-co/strings.xml -OutFile D:\Temp\components-feature-addons-strings.xml
+Invoke-WebRequest -Uri https://github.com/mozilla-l10n/android-l10n/raw/master/mozilla-mobile/android-components/components/feature/app-links/src/main/res/values-co/strings.xml -OutFile D:\Temp\components-feature-app-links-strings.xml
+Invoke-WebRequest -Uri https://github.com/mozilla-l10n/android-l10n/raw/master/mozilla-mobile/android-components/components/feature/autofill/src/main/res/values-co/strings.xml -OutFile D:\Temp\components-feature-autofill-strings.xml
+Invoke-WebRequest -Uri https://github.com/mozilla-l10n/android-l10n/raw/master/mozilla-mobile/android-components/components/feature/awesomebar/src/main/res/values-co/strings.xml -OutFile D:\Temp\components-feature-awesomebar-strings.xml
+Invoke-WebRequest -Uri https://github.com/mozilla-l10n/android-l10n/raw/master/mozilla-mobile/android-components/components/feature/contextmenu/src/main/res/values-co/strings.xml -OutFile D:\Temp\components-feature-contextmenu-strings.xml
+Invoke-WebRequest -Uri https://github.com/mozilla-l10n/android-l10n/raw/master/mozilla-mobile/android-components/components/feature/customtabs/src/main/res/values-co/strings.xml -OutFile D:\Temp\components-feature-customtabs-strings.xml
+Invoke-WebRequest -Uri https://github.com/mozilla-l10n/android-l10n/raw/master/mozilla-mobile/android-components/components/feature/downloads/src/main/res/values-co/strings.xml -OutFile D:\Temp\components-feature-downloads-strings.xml
+Invoke-WebRequest -Uri https://github.com/mozilla-l10n/android-l10n/raw/master/mozilla-mobile/android-components/components/feature/findinpage/src/main/res/values-co/strings.xml -OutFile D:\Temp\components-feature-findinpage-strings.xml
+Invoke-WebRequest -Uri https://github.com/mozilla-l10n/android-l10n/raw/master/mozilla-mobile/android-components/components/feature/media/src/main/res/values-co/strings.xml -OutFile D:\Temp\components-feature-media-strings.xml
+Invoke-WebRequest -Uri https://github.com/mozilla-l10n/android-l10n/raw/master/mozilla-mobile/android-components/components/feature/privatemode/src/main/res/values-co/strings.xml -OutFile D:\Temp\components-feature-privatemode-strings.xml
+Invoke-WebRequest -Uri https://github.com/mozilla-l10n/android-l10n/raw/master/mozilla-mobile/android-components/components/feature/prompts/src/main/res/values-co/strings.xml -OutFile D:\Temp\components-feature-prompts-strings.xml
+Invoke-WebRequest -Uri https://github.com/mozilla-l10n/android-l10n/raw/master/mozilla-mobile/android-components/components/feature/pwa/src/main/res/values-co/strings.xml -OutFile D:\Temp\components-feature-pwa-strings.xml
+Invoke-WebRequest -Uri https://github.com/mozilla-l10n/android-l10n/raw/master/mozilla-mobile/android-components/components/feature/qr/src/main/res/values-co/strings.xml -OutFile D:\Temp\components-feature-qr-strings.xml
+Invoke-WebRequest -Uri https://github.com/mozilla-l10n/android-l10n/raw/master/mozilla-mobile/android-components/components/feature/readerview/src/main/res/values-co/strings.xml -OutFile D:\Temp\components-feature-readerview-strings.xml
+Invoke-WebRequest -Uri https://github.com/mozilla-l10n/android-l10n/raw/master/mozilla-mobile/android-components/components/feature/search/src/main/res/values-co/strings.xml -OutFile D:\Temp\components-feature-search-strings.xml
+Invoke-WebRequest -Uri https://github.com/mozilla-l10n/android-l10n/raw/master/mozilla-mobile/android-components/components/feature/sitepermissions/src/main/res/values-co/strings.xml -OutFile D:\Temp\components-feature-sitepermissions-strings.xml
+Invoke-WebRequest -Uri https://github.com/mozilla-l10n/android-l10n/raw/master/mozilla-mobile/android-components/components/feature/tabs/src/main/res/values-co/strings.xml -OutFile D:\Temp\components-feature-tabs-strings.xml
+Invoke-WebRequest -Uri https://github.com/mozilla-l10n/android-l10n/raw/master/mozilla-mobile/android-components/components/feature/webnotifications/src/main/res/values-co/strings.xml -OutFile D:\Temp\components-feature-webnotifications-strings.xml
+Invoke-WebRequest -Uri https://github.com/mozilla-l10n/android-l10n/raw/master/mozilla-mobile/android-components/components/lib/crash/src/main/res/values-co/strings.xml -OutFile D:\Temp\components-lib-crash-strings.xml
+Invoke-WebRequest -Uri https://github.com/mozilla-l10n/android-l10n/raw/master/mozilla-mobile/android-components/components/service/nimbus/src/main/res/values-co/strings.xml -OutFile D:\Temp\components-service-nimbus-strings.xml
+Invoke-WebRequest -Uri https://github.com/mozilla-l10n/android-l10n/raw/master/mozilla-mobile/android-components/components/support/base/src/main/res/values-co/strings.xml -OutFile D:\Temp\components-support-base-strings.xml
+Invoke-WebRequest -Uri https://github.com/mozilla-l10n/android-l10n/raw/master/mozilla-mobile/android-components/components/support/ktx/src/main/res/values-co/strings.xml -OutFile D:\Temp\components-support-ktx-strings.xml
+Invoke-WebRequest -Uri https://github.com/mozilla-l10n/android-l10n/raw/master/mozilla-mobile/android-components/components/support/migration/src/main/res/values-co/strings.xml -OutFile D:\Temp\components-support-migration-strings.xml
+Invoke-WebRequest -Uri https://github.com/mozilla-l10n/android-l10n/raw/master/mozilla-mobile/android-components/components/ui/tabcounter/src/main/res/values-co/strings.xml -OutFile D:\Temp\components-ui-tabcounter-strings.xml
+Invoke-WebRequest -Uri https://github.com/mozilla-l10n/android-l10n/raw/master/mozilla-mobile/fenix/app/src/main/res/values-co/strings.xml -OutFile D:\Temp\fenix-strings.xml
